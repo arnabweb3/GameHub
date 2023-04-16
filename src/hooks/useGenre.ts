89dd6125d2f2da2497169dc2a5ea685e3ecaeabc
Arnab Banerjee <arnabweb3.0@gmail.com>
2023-04-16 17:02:09 +0530

    Ship genres with app

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+// import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -7,5 +8,6 @@ export interface Genre {
 }
 
 const useGenre = () => useData<Genre>("/genres");
+// const useGenre = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenre;
